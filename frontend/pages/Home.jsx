66c5a296a6b0e894697d151875cb05f9f101aede
snehildwivedi03 <snehildwivedi03@gmail.com>
2025-09-19 18:00:43 +0530

    Package.json file updated node version

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,6 +1,5 @@
 // src/pages/Home.jsx
 import React from "react";
-import { Link } from "react-router-dom";
 
 const Home = () => {
   return (
@@ -15,18 +14,18 @@ const Home = () => {
         </p>
 
         <div className="flex flex-col sm:flex-row gap-4 justify-center">
-          <Link
-            to="/login"
+          <a
+            href="/login"
             className="px-6 py-3 rounded-lg bg-blue-600 text-white font-medium hover:bg-blue-700 transition"
           >
             Login
-          </Link>
-          <Link
-            to="/register"
+          </a>
+          <a
+            href="/register"
             className="px-6 py-3 rounded-lg bg-gray-200 text-gray-800 font-medium hover:bg-gray-300 transition dark:bg-gray-700 dark:text-white dark:hover:bg-gray-600"
           >
             Register
-          </Link>
+          </a>
         </div>
       </div>
     </section>
